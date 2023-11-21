7f0dcc0c02624b4edbf02c43de08db1d8dc0393c
Japhet1 <40357151+Japhet1@users.noreply.github.com>
2023-11-21 00:28:04 +0000

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import { BsInstagram, BsTwitter, BsGithub, BsLinkedin, BsTiktok } from "react-icons/bs";
 import { BsDownload } from "react-icons/bs";
+
 function Header({ headerData }) {
   return (
     <main id="about" className="text-center font-gruppo items-center py-10 md:py-10 lg:px-32 lg:py-16 border-b border-gray-200 md:mx-10 dark:border-gray-900 scroll-mt-28 scroll-smooth">
@@ -23,7 +24,7 @@ function Header({ headerData }) {
         
         <div className="flex justify-center items-center mt-5 p-5 gap-10">
           <button><a className="flex justify-center items-bottom gap-4 md:px-10 md:py-2 p-2 text-md md:text-lg lg:text-xl bg-blue-950 lg:hover:bg-blue-100 hover:shadow-lg text-white hover:text-blue-950 transition-all rounded-md font-semibold" href="https://github.com/Japhet1" target="_blank" rel="noreferrer"><BsGithub className="text-xl md:text-2xl lg:text-2xl"/>Repository</a></button>
-          <button><a className="flex justify-center items-bottom gap-4 md:px-10 md:py-2 p-2 text-md md:text-lg lg:text-xl bg-blue-950 lg:hover:bg-blue-100 hover:shadow-lg text-white hover:text-blue-950 transition-all rounded-md font-semibold" href="https://github.com/Japhet1" target="_blank" rel="noreferrer">Resume<BsDownload className="text-xl md:text-2xl lg:text-2xl"/></a></button>
+          <button><a className="flex justify-center items-bottom gap-4 md:px-10 md:py-2 p-2 text-md md:text-lg lg:text-xl bg-blue-950 lg:hover:bg-blue-100 hover:shadow-lg text-white hover:text-blue-950 transition-all rounded-md font-semibold" href="/Passah-Japhet-CV.pdf" download="Passah-Japhet-CV.pdf">Resume<BsDownload className="text-xl md:text-2xl lg:text-2xl"/></a></button>
         </div>
       </div>
     </main>
