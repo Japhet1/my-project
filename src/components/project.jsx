13b96d25fa7447ccefbf065924dd28bf02d360ba
Japhet1 <40357151+Japhet1@users.noreply.github.com>
2024-02-04 20:19:33 +0000

    redesign the project section

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -8,25 +8,25 @@ const Project = ({ projectData }) => {
         </div>
         <div className="my-16 space-y-10 md:p-10 md:text-center">
             {projectData.map((project, index) => (
-                <div key={index} className="flex mb-10 md:mb-16 lg:mb-0 dark:text-white lg:bg-green-50 dark:lg:bg-cyan-900 rounded-md lg:hover:shadow-2xl">
+                <div key={index} className="flex p-10 mb-10 md:mb-16 lg:mb-0 dark:text-white rounded-md">
                    
                     <div className="grid grid-flow-col">
                         <div className="col-span-4">
                             <div className="md:max-w-lg lg:w-full rounded-md transition-all bg-inherit" style={{}}>
-                                <img className="lg:w-full lg:h-72 rounded-md" src={`/${project.src}`} alt="first project" />
+                                <img className="border lg:w-full lg:h-72 rounded-md" src={`/${project.src}`} alt="first project" />
                             </div>
                         </div>
                         
                  
-                        <div className="col-span-6 pt-10 px-10 lg:h-72">
+                        <div className="col-span-6 items-center px-10 lg:h-72">
                             <div><h1 className="text-green-800 dark:text-white text-xl md:text-2xl lg:text-2xl font-bold">{project.name}</h1></div>
-                            <div className="mt-4 px-4">
+                            <div className="mt-4 px-20">
                                 <p className="text-md md:text-lg lg:text-lg text-slate-600 dark:text-slate-300 text-center leading-8 font-normal">{project.description}</p>
                             </div>
                             
                             <div className="flex flex-wrap my-4 md:my-10 lg:my-4 px-4 gap-2 justify-center items-center md:text-sm lg:text-sm">
                                 <div className="font-bold text-xl"><h1>Stack:</h1></div>
-                                {project.stack.map((project, id) => <h1 key={id} className="py-1 px-2 bg-green-100 dark:bg-cyan-800  md:bg-green-100 md:dark:bg-green-800 lg:bg-white lg:dark:bg-green-800 rounded-md font-normal">{project.item}</h1>)}
+                                {project.stack.map((project, id) => <h1 key={id} className="py-1 px-2 bg-green-100 dark:bg-cyan-800  md:bg-green-100 md:dark:bg-green-800 lg:bg-green-100 lg:dark:bg-green-800 rounded-md font-normal">{project.item}</h1>)}
                             </div>
                             <div className="flex md:flex lg:flex justify-center my-5">
                                 <button><a className="py-2 px-4 space-x-2 flex md:p-2 lg:px-4 items-bottom text-green-800 font-semibold dark:text-white md:text-md transition-all lg:text-md" href={project.code} target="_blank" rel="noreferrer"><h1>Source Code</h1><BsBoxArrowUpRight/></a></button>
